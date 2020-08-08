a6ef63c287cc9a45357d7ec8a3bb77de45687de6
Venkatesh Raman <rambo.saimukunth@gmail.com>
2020-08-08 19:49:30 +0530

    Adding comment to UDF Function

--- src/main/scala/hiveudf/SimpleUDFExample.java
+++ src/main/scala/hiveudf/SimpleUDFExample.java
@@ -9,6 +9,9 @@ import org.apache.hadoop.io.Text;
         value = "returns 'hello x', where x is whatever you give it (STRING)",
         extended = "SELECT simpleudfexample('world') from foo limit 1;"
 )
+
+// Simple Hive UDF
+
 public class SimpleUDFExample extends UDF {
 
     public Text evaluate(Text input) {
